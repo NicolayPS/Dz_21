f2ee69c199ace1175f4d1b64a3b9cd871ae60fd8
oksanaPC2 <77opod@gmail.com>
2022-08-20 17:17:08 +0300

    0

--- Dz_21/Program.cs
+++ Dz_21/Program.cs
@@ -53,9 +53,9 @@ namespace Dz_21
         }
         static void Gardner2()
         {
-            for (int i = m - 1; i > 0; i--)
+            for (int i = m - 1; i >= 0; i--)
             {
-                for (int j = n - 1; j > 0; j--)
+                for (int j = n - 1; j >= 0; j--)
                 {
                     if (path[j, i] == 0)
                     {
